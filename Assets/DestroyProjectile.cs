7d963560136366a484e7dc18fca9a446a1aaaffb
ZCBML13 <69592879+ZCBML13@users.noreply.github.com>
2023-01-27 09:35:11 -0500

    Update DestroyProjectile.cs

--- Assets/DestroyProjectile.cs
+++ Assets/DestroyProjectile.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 
 public class DestroyProjectile : MonoBehaviour
 {
+    // variable for the width of the screen
     public float ScreenWidth = 12.5f;
+    // variable for the player gameobject
     public GameObject shield;
+    // variable for the shield script
     public UseShield use;
-
+    
+    // When the game object is created it finds the shield script from the player gameobject
     private void Awake()
     {
         shield = GameObject.FindWithTag("Player");
@@ -18,12 +22,14 @@ public class DestroyProjectile : MonoBehaviour
 
     void Update()
     {
+        // if the projectile goes our side of the cameras FOV it gets destroyed
         if (transform.position.x > Camera.main.transform.position.x + ScreenWidth || transform.position.x < Camera.main.transform.position.x - ScreenWidth)
         {
             Destroy(gameObject);
         }
     }
 
+    // If the players projectille hits an enemy it is destroyed
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(gameObject.tag == "AllayMagic" || gameObject.tag == "AllayDart")
@@ -33,6 +39,7 @@ public class DestroyProjectile : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+        // If an enemies projectile hits the player while they aren't blocking it is destroyed
         if (gameObject.tag == "EnemyMagic" || gameObject.tag == "EnemyDart")
         {
             if (!use.blocking)
@@ -44,6 +51,7 @@ public class DestroyProjectile : MonoBehaviour
             }
             
         }
+        // if any projectile hits the floor it is destroyed
         if (collision.gameObject.tag == "Floor")
         {
             Destroy(gameObject);
