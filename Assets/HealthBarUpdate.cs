e0596bec9ee7e814f60e98fce9f5aa9a3ba77167
ZCBML13 <69592879+ZCBML13@users.noreply.github.com>
2023-01-27 09:38:58 -0500

    Update HealthBarUpdate.cs

--- Assets/HealthBarUpdate.cs
+++ Assets/HealthBarUpdate.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class HealthBarUpdate : MonoBehaviour
 {
+    // Variable for the max health a player could have
     private const float Max_Health = 100;
 
+    // float for the health of the player right now
     private float health;
 
+    // variable for the healthbar on your screen
     private Image healthBar;
 
+    // variable for the healthbar script
     public HealthBar heal;
 
     // Start is called before the first frame update
@@ -22,6 +26,7 @@ public class HealthBarUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // every frame the image on your screen is updated with the current health of the player by divding it by the total health then filling the image by that percentage
         health = heal.Health;
         healthBar.fillAmount = health / Max_Health;
     }
